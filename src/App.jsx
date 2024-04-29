70dde73dc8e97e5f86decfe258e13b71ffd5cb40
Eduardo Lopes <montalverne.edu@gmail.com>
2024-04-29 11:29:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  
+  return (
+    <div>
+      olá, react 
+    </div>
+  )
+}
